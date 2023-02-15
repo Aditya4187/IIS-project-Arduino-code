3cabde87e32956f1f78bdecbd9d47f72c6e61ad6
Aditya4187 <125042163+Aditya4187@users.noreply.github.com>
2023-02-15 19:18:42 +0530

    Add files via upload

---
+++ IR+VB_check_code.py
@@ -0,0 +1,26 @@
+from machine import Pin
+from machine import ADC
+from time import sleep
+
+led = Pin(4,Pin.OUT)
+sensor = ADC(0)
+solenoid = Pin(5,Pin.OUT)
+threshold = 50
+ir_sensor_pin = Pin(2, Pin.IN)  # set up the IR sensor pin
+
+def loop():
+    value = sensor.read()
+    ir_value = ir_sensor_pin.value()  # read the IR sensor value
+    if value >= threshold and ir_value == 1:  # only trigger if both sensor values are met
+        led.value(0)
+        solenoid.value(1)
+        sleep(1)
+    else:
+        led.value(1)
+        solenoid.value(0)
+        sleep(1)
+
+while True:
+    loop()
+
+        
